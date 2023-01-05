009096cfb8417ef7957017f44f67bb464ef5a0b5
Jhonny-32 <jhoapoloche@misena.edu.co>
2023-01-05 12:39:06 -0500

    update gradle
    update gradle

--- app/src/main/java/com/edifice/residentialsecurity/data/model/User.kt
+++ app/src/main/java/com/edifice/residentialsecurity/data/model/User.kt
@@ -3,6 +3,8 @@ package com.edifice.residentialsecurity.data.model
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import javax.inject.Inject
+import javax.inject.Singleton
+
 
 class User(
     @SerializedName("id") val id: String? = null,
@@ -17,8 +19,8 @@ class User(
     @SerializedName("conjunto") val conjunto: String? = null,
     @SerializedName("roles") val roles: ArrayList<Rol>? = null,
 
-    @SerializedName("tower") val tower: String ? = null,
-    @SerializedName("apartament") val apartament: String? = null
+    @SerializedName("tower") var tower: String ? = null,
+    @SerializedName("apartament") var apartament: String? = null
 ) {
     fun getFullName(): String = "$name $lastname"
     override fun toString(): String {
@@ -28,6 +30,12 @@ class User(
     fun isNotEmpty() = name.isNotEmpty() && lastname.isNotEmpty() && phone.isNotEmpty() && email.isNotEmpty() && dni.isNotEmpty() && password.isNotEmpty()
 
 
-
-
 }
+
+data class UserDataResidential(
+    @SerializedName("name") var name: String,
+    @SerializedName("phone") var phone: String,
+    @SerializedName("tower") var tower: String ? = null,
+    @SerializedName("apartament") var apartament: String? = null
+)
+
